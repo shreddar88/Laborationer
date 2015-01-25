2984d7cf5f54cd023283977569814eff59356a31
Asgeir Sverrisson <asgeir_Sverrisson@hotmail.com>
2015-01-25 13:10:26 +0100

    LABB 4

---
+++ Lab4/Laboration4.A/SecretNumber.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+
+namespace Laboration4.A
+{
+    public class SecretNumber
+    {
+        private int _count;
+
+        private int _number;
+
+        public const int MaxNumberOfGuesses = 7;
+
+        public void Initialize()
+        {
+            Random rnd = new Random();
+            _number = rnd.Next(1, 100);
+            _count = 0;       
+        }
+
+        public bool MakeGuess(int number)
+        {
+            if (_count >= MaxNumberOfGuesses)
+            {
+                throw new ApplicationException();
+
+            }
+
+            if (number >= 101 || number <= 0)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+            if (number < _number)
+            {
+                Console.WriteLine("{0} är för lågt! Du har {1} gissningar kvar!", number, MaxNumberOfGuesses - (_count + 1));
+                _count++;
+
+                    if (_count == MaxNumberOfGuesses)
+                    {
+                        Console.WriteLine("Det hemliga talet är {0}.", _number);
+                    }
+
+                return false;
+            }
+
+            if (number > _number)
+            {
+                Console.WriteLine("{0} är för högt. Du har {1} gissningar kvar.", number, MaxNumberOfGuesses - (_count + 1));
+                _count++;
+
+                    if (_count == MaxNumberOfGuesses)
+                    {
+                        Console.WriteLine("Det hemliga talet är {0}.", _number);
+                    }
+                    return false;
+            }
+           
+                Console.WriteLine("Härligt Jobbat! Du klarade det på {0} försök.", _count + 1);
+                Console.WriteLine("Det hemliga talet var {0}.", _number);
+                return true;
+            }
+        
+        public SecretNumber()
+        {
+            Initialize();
+        }
+    }
+}
