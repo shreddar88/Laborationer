c6c55797300634676b72594549d01969097af9b8
Asgeir Sverrisson <asgeir_Sverrisson@hotmail.com>
2015-02-23 10:14:51 +0100

    Korrigerat lab 3

--- LoneRevision/LoneRevision/Program.cs
+++ LoneRevision/LoneRevision/Program.cs
@@ -12,9 +12,9 @@ namespace LoneRevision
         {
             int antalLoner = 0;
 
-         do
-         {     
-           antalLoner = ReadInt("Ange antal löner att mata in: ");
+            do
+            {
+                antalLoner = ReadInt("Ange antal löner att mata in: ");
 
                 if (antalLoner <= 1)
                 {
@@ -25,30 +25,39 @@ namespace LoneRevision
 
                 else
                 {
-                   processSalaries(antalLoner);
+                    ProcessSalaries(antalLoner);
                 }
-                    Console.BackgroundColor = ConsoleColor.DarkGreen;
-                    Console.WriteLine("\nTryck ner valfri tangent för ny beräkning - ESC avslutar");
-                    Console.ResetColor();
-         } while (Console.ReadKey(true).Key != ConsoleKey.Escape);
+                Console.BackgroundColor = ConsoleColor.DarkGreen;
+                Console.WriteLine("\nTryck ner valfri tangent för ny beräkning - ESC avslutar");
+                Console.ResetColor();
+            } while (Console.ReadKey(true).Key != ConsoleKey.Escape);
         }
-     
 
-     static int ReadInt(string prompt)
+
+        static int ReadInt(string prompt)
         {
             string nummer = string.Empty;
-            int antalLoner = 0;
+            int tal = 0;
 
-            while (true) 
+            while (true)
             {
                 try
                 {
                     Console.Write(prompt);
                     nummer = Console.ReadLine();
-                    antalLoner = int.Parse(nummer);
-                    break;
-                }
+                    tal = int.Parse(nummer);
 
+
+                    if (tal <= 0)
+                    {
+                        Console.BackgroundColor = ConsoleColor.Red;
+                        Console.ForegroundColor = ConsoleColor.White;
+                        Console.WriteLine("FEL! '{0}' kan inte tolkas som ett positivt heltal!", nummer);
+                        Console.ResetColor();
+                        
+                    }
+                    return tal;
+                }
                 catch (Exception)
                 {
                     Console.BackgroundColor = ConsoleColor.Red;
@@ -57,63 +66,66 @@ namespace LoneRevision
                     Console.ResetColor();
                 }
             }
-            return antalLoner;
         }
+            
 
-        static void processSalaries(int antalLoner)
+        static void ProcessSalaries(int antalLoner)
         {
-            string prompt = "Ange lön nummer ";
             int[] loner = new int[antalLoner];
             Console.WriteLine();
+
             for (int i = 0; i < antalLoner; i++) // Loopar antalet löner och sparar värden i en array
-            {
-                loner[i] = ReadInt(prompt + (i + 1) + ": ");
-            }
+            {            
+                    string prompt = string.Format("Ange lön nummer {0}: ", i + 1);            
+                        loner[i] = ReadInt(prompt);
+               
+            }                
+
 
             int[] sortLoner = new int[antalLoner];
 
             Array.Copy(loner, sortLoner, antalLoner); // Kopierar nuvarande array till ny array och sorterar den
-                    Array.Sort(sortLoner);
-                    
-           double medianLon = 0;
-           double medelLon = sortLoner.Average();
-           int loneSpridning = sortLoner.Max() - sortLoner.Min();
+            Array.Sort(sortLoner);
 
-           if (sortLoner.Length % 2 == 1) // Räknar ut medianlön vid udda
-              {
+            decimal medianLon = 0;
+            double medelLon = sortLoner.Average();
+            int loneSpridning = sortLoner.Max() - sortLoner.Min();
+
+            if (sortLoner.Length % 2 == 1) // Räknar ut medianlön vid udda
+            {
                 medianLon = sortLoner[sortLoner.Length / 2];
-              }
+            }
 
-           else // Räknar ut medianlön vid jämna
-             {
-                   int median1 = sortLoner[sortLoner.Length / 2];
-                   int median2 = sortLoner[sortLoner.Length / 2 - 1];
-                   medianLon = (median1 + median2) / 2;
-              }
-                    Console.WriteLine();
+            else // Räknar ut medianlön vid jämna
+            {
+                int median1 = sortLoner[sortLoner.Length / 2];
+                int median2 = sortLoner[sortLoner.Length / 2 - 1];
+                medianLon = (median1 + median2) / 2.0m;
+            }
+            Console.WriteLine();
 
-                    Console.WriteLine("------------------------------------------");
-                    Console.WriteLine("\n{0}     : {1,9 :C0}", "Medianlön", medianLon);
-                    Console.WriteLine("{0}      : {1,9 :C0}", "Medellön", medelLon);
-                    Console.WriteLine("{0} : {1,9 :C0}\n", "Lönespridning", loneSpridning);
-                    Console.WriteLine("------------------------------------------");
+            Console.WriteLine("------------------------------------------");
+            Console.WriteLine("\n{0}     : {1,9 :C0}", "Medianlön", medianLon);
+            Console.WriteLine("{0}      : {1,9 :C0}", "Medellön", medelLon);
+            Console.WriteLine("{0} : {1,9 :C0}\n", "Lönespridning", loneSpridning);
+            Console.WriteLine("------------------------------------------");
 
-               int raknare = 0;
+            int raknare = 0;
 
-               foreach (int loop in loner)//Loopar första arrayen och presenterar löner högerjusterade tre per rad i den ordning de matats in.
-               {
-                   if (raknare % 3 == 0)
-                   {
-                       Console.Write("\n{0,8}", loop);
-                       raknare++;
-                   }
-                   else
-                   {
-                       Console.Write("{0,8}", loop);
-                       raknare++;
-                   }
-               }
-               Console.WriteLine();
-          }
-      }
- }
+            foreach (int loop in loner)//Loopar första arrayen och presenterar löner högerjusterade tre per rad i den ordning de matats in.
+            {
+                if (raknare % 3 == 0)
+                {
+                    Console.Write("\n{0,8}", loop);
+                    raknare++;
+                }
+                else
+                {
+                    Console.Write("{0,8}", loop);
+                    raknare++;
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
